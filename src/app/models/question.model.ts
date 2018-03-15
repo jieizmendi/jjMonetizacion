acbfb9882aa14f081047fa833d6cbb2db5f232f3
Juan Emilio Nieva Casanova <juan.nieva94@gmail.com>
2018-03-15 12:26:40 -0300

    tildes y puntuacion

--- src/app/models/question.model.ts
+++ src/app/models/question.model.ts
@@ -3,16 +3,16 @@ export class Question {
 }
 
 export const QUESTIONS: Question[] = [
-    new Question("Cual de estas opciones no es una implementacion de estrategia de monetizacion?", ["Todas son implementaciones de estrategias validas", "Banner publicitario","Niveles especiales", "Reduccion de tiempo de espera","Venta de personajes o items"]),
-    new Question("Cuales de estas opciones no es una estrategia de monetizacion dentro de los videojuegos?", ["Separacion", "Restriccion", "Personalizacion", "Jugabilidad"]),    
-    new Question("Que estrategia es la mas rapida de implementar y monetizar?", ["Restriccion", "Personalizacion", "Jugabilidad", "Separacion"]),
-    new Question("Que estrategia es la ideal para videojuegos casuales?", ["Restriccion", "Personalizacion", "Jugabilidad", "Separacion"]),
-    new Question("Que estrategia se utiliza en videojuegos que necesitan monetizar muy rapido a sus jugadores antes que dejen de jugar?", ["Restriccion", "Personalizacion", "Jugabilidad", "Separacion"]),
-    new Question("La utilizacion de la publicidad voluntaria como \"pago\" se da en la estrategia... ", ["Restriccion", "Personalizacion", "Jugabilidad", "Separacion"]),
-    new Question("Los cosmeticos en los videojuegos a que estrategia pertenecen?", ["Personalizacion", "Restriccion", "Jugabilidad", "Separacion"]),
-    new Question("Que estrategia aporta buenos ingresos y es muy atractiva aunque es larga y dificil y en definitiva se necesita mucho tiempo?", ["Personalizacion", "Restriccion", "Jugabilidad", "Separacion"]),
-    new Question("Que estrategia de monetizacion agrega nuevas posibilidades y combinaciones de juego?", ["Jugabilidad", "Restriccion", "Personalizacion", "Separacion"]),
-    new Question("Que estrategia es mas dificil de aplicar por que afecta las mecanicas del videojuego?", ["Jugabilidad", "Restriccion", "Personalizacion", "Separacion"]),
-    new Question("Que estrategia de monetizacion le permite al jugador disfrutar de nuevos escenarios,mapas, objetos, etc.", ["Jugabilidad", "Restriccion", "Personalizacion", "Separacion"]),
-    new Question("Hay alguna estrategia mejor que otra?", ["No, todas tienen sus ventajas y desventajas", "Si, la separacion es la mas importante", "Si, la restriccion ya que no se pueden combinar diferentes estrategias", "Si pero depende del tipo de videojuego"])
+    new Question("¿Cuál de estas opciones no es una implementación de estrategia de monetización?", ["Todas son implementaciones de estrategias validas", "Banner publicitario","Niveles especiales", "Reducción de tiempo de espera","Venta de personajes o ítems"]),
+    new Question("¿Cuáles de estas opciones no es una estrategia de monetización dentro de los videojuegos?", ["Separación", "Restricción", "Personalización", "Jugabilidad"]),    
+    new Question("¿Qué estrategia es la mas rápida de implementar y monetizar?", ["Restricción", "Personalización", "Jugabilidad", "Personalización"]),
+    new Question("¿Qué estrategia es la ideal para videojuegos casuales?", ["Restricción", "Personalización", "Jugabilidad", "Separación"]),
+    new Question("¿Qué estrategia se utiliza en videojuegos que necesitan monetizar muy rápido a sus jugadores antes que dejen de jugar?", ["Restricción", "Personalización", "Jugabilidad", "Separación"]),
+    new Question("La utilización de la publicidad voluntaria como \"pago\" se da en la estrategia... ", ["Restricción", "Personalización", "Jugabilidad", "Separación"]),
+    new Question("Los cosméticos en los videojuegos, ¿A que estrategia pertenecen?", ["Personalización", "Restricción", "Jugabilidad", "Separación"]),
+    new Question("¿Qué estrategia aporta buenos ingresos y es muy atractiva aunque es larga y difícil y en definitiva se necesita mucho tiempo?", ["Personalización", "Restricción", "Jugabilidad", "Separación"]),
+    new Question("¿Qué estrategia de monetización agrega nuevas posibilidades y combinaciones de juego?", ["Jugabilidad", "Restricción", "Personalización", "Separación"]),
+    new Question("¿Qué estrategia es mas difícil de aplicar por que afecta las mecánicas del videojuego?", ["Jugabilidad", "Restricción", "Personalización", "Separación"]),
+    new Question("¿Qué estrategia de monetización le permite al jugador disfrutar de nuevos escenarios,mapas, objetos, etc.", ["Jugabilidad", "Restricción", "Personalización", "Separación"]),
+    new Question("¿Hay alguna estrategia mejor que otra?", ["No, todas tienen sus ventajas y desventajas", "Si, la Separación es la mas importante", "Si, la restricción ya que no se pueden combinar diferentes estrategias", "Si pero depende del tipo de videojuego"])
 ];
